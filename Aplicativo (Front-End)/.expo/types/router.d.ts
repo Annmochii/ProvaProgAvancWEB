e08365e388683c2c54ee131ae25944ae3848f29e
Anna Maria Lima <anninhalimas22@gmail.com>
2024-11-21 07:51:26 -0300

    add back-end

--- Aplicativo (Front-End)/.expo/types/router.d.ts
+++ Aplicativo (Front-End)/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/users` | `/_sitemap` | `/signin` | `/signup` | `/users`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/users` | `/..\Aplicativo\constants\Colors` | `/..\Aplicativo\hooks\useColorScheme` | `/..\Aplicativo\hooks\useColorScheme.web` | `/..\Aplicativo\hooks\useThemeColor` | `/_sitemap` | `/signin` | `/signup` | `/users`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
